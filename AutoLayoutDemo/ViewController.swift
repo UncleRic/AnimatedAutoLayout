18fdfadbc8d36aca42943c9cdf5a38adf67fd94d
Frederick C. Lee <frederick.c.lee@gmail.com>
2016-03-03 12:57:36 -0800

    Code-Logic Cleanup.

--- AutoLayoutDemo/ViewController.swift
+++ AutoLayoutDemo/ViewController.swift
@@ -1,19 +1,20 @@
-//
-//  ViewController.swift
+//  MainViewController.swift
 //  AutoLayoutDemo
 //
 //  Created by Frederick C. Lee on 1/17/15.
 //  Copyright (c) 2015 Frederick C. Lee. All rights reserved.
-//
+// -----------------------------------------------------------------------------------------------------
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var widthConstraint: NSLayoutConstraint!
     @IBOutlet weak var verticalConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var redView: UIView!
+    
+    static var staticToggle:Bool = true
     
     // -----------------------------------------------------------------------------------------------------
     
@@ -32,40 +33,40 @@ class ViewController: UIViewController {
     
     var step:CGFloat = 1.0 as CGFloat
     
-    func startAnimation() {
+    func startRotation() {
         self.view.layoutIfNeeded()
-        var shit:Bool = true
+        
         UIView.animateWithDuration(0.5, delay:0.0,
-                                   options:.CurveLinear,
-                                   animations: {
-                                    if shit {
-                                        self.redView.transform = CGAffineTransformMakeRotation(36);
-                                    } else {
-                                        self.redView.transform = CGAffineTransformMakeRotation(72);
-                                    }
-                                    self.view.layoutIfNeeded()
+               options:.CurveLinear,
+               animations: {
+                if MainViewController.staticToggle {
+                    self.redView.transform = CGAffineTransformMakeRotation(36);
+                } else {
+                    self.redView.transform = CGAffineTransformMakeRotation(-36);
+                }
+                self.view.layoutIfNeeded()
             },
-                                   completion: {(finished:Bool) in
-                                    if finished {
-                                        if shit {
-                                            self.startAnimation()
-                                            shit = false
-                                        }
-                                    }
+           completion: {(finished:Bool) in
+            if finished {
+                MainViewController.staticToggle = !MainViewController.staticToggle
+            }
         })
         
     }
     
     
-    
     // -----------------------------------------------------------------------------------------------------
     // Mark: - Action methods
     
-    @IBAction func doSomethingAction(sender: UIButton) {
-        startAnimation()
+    
+    @IBAction func rotateAction(sender: UIBarButtonItem) {
+        startRotation()
+    }
+    @IBAction func doorAction(sender: UIBarButtonItem) {
+        print("Door Action.")
     }
     
-    @IBAction func exitAction(sender: UIButton) {
+    @IBAction func exitAction(sender: UIBarButtonItem) {
         exit(0)
     }
     
